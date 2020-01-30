7d1ff2e4ece7db94e8efcc9486fb15be1ed3625e
gpinto <giuseppe.pinto@lastminute.com>
2020-01-30 12:50:42 +0100

    fix chain of responsibility pattern

--- behavioral-design-patterns/src/main/java/chainofresponsibility/ChainOfResponsibilityDemo.java
+++ behavioral-design-patterns/src/main/java/chainofresponsibility/ChainOfResponsibilityDemo.java
@@ -5,10 +5,13 @@ import chainofresponsibility.processor.OAuthProcessor;
 import chainofresponsibility.processor.UsernameAndPasswordProcessor;
 import chainofresponsibility.provider.OAuthTokenProvider;
 import chainofresponsibility.provider.SamlTokenProvider;
+import chainofresponsibility.provider.UsernameAndPasswordProvider;
 
 public class ChainOfResponsibilityDemo
 {
   public static final String TOKEN = "token";
+  public static final String USERNAME = "username";
+  public static final String PASSWORD = "password";
 
   public static void main(String[] args)
   {
@@ -16,6 +19,9 @@ public class ChainOfResponsibilityDemo
 
     boolean authorized = authProcessorChain.isAuthorized(new OAuthTokenProvider(TOKEN));
     System.out.println("With the OAuthTokenProvider is authorized? " + authorized);
+
+    authorized = authProcessorChain.isAuthorized(new UsernameAndPasswordProvider(USERNAME, PASSWORD));
+    System.out.println("With the UsernameAndPasswordProvider is authorized? " + authorized);
 
     authorized = authProcessorChain.isAuthorized(new SamlTokenProvider());
     System.out.println("With the SamlTokenProvider is authorized? " + authorized);
