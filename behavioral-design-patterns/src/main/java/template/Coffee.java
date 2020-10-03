1763926fdc010333aadc2731f64fe81189fb1df1
gpinto <giuseppe.pinto@lastminute.com>
2020-10-03 12:41:15 +0200

    template

---
+++ behavioral-design-patterns/src/main/java/template/Coffee.java
@@ -0,0 +1,29 @@
+package template;
+
+public class Coffee
+{
+  void prepareRecipe()
+  {
+    boilWater();
+    brewCoffeeGrinds();
+    pourInCup();
+    addSugarAndMilk();
+  }
+
+  
+  private void addSugarAndMilk()
+  {
+  }
+
+  private void pourInCup()
+  {
+  }
+
+  private void brewCoffeeGrinds()
+  {
+  }
+
+  private void boilWater()
+  {
+  }
+}
